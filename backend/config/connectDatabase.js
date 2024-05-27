38066289b086112d009ce23719f835b799026e17
Manikandan <manikandanbca87@gmail.com>
2024-05-27 18:16:23 +0530

    cmd

--- backend/config/connectDatabase.js
+++ backend/config/connectDatabase.js
@@ -7,21 +7,3 @@ const connectDatabase = () => {
 };
 
 module.exports = connectDatabase;
-
-// const mongoose = require('mongoose');
-
-// const connectDatabase = () => {
-//     return new Promise((resolve, reject) => {
-//         mongoose.connect(process.env.DB_URL)
-//         .then((con) => {
-//             console.log("MongoDB connected to host: " + con.connection.host);
-//             resolve();
-//         })
-//         .catch((err) => {
-//             console.error("MongoDB connection error:", err);
-//             reject(err);
-//         });
-//     });
-// };
-
-// module.exports = connectDatabase;
