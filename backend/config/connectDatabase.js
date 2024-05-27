f8ac64793b979d01483272d53a9e85cf66749c03
Manikandan <manikandanbca87@gmail.com>
2024-05-27 17:58:03 +0530

    db

--- backend/config/connectDatabase.js
+++ backend/config/connectDatabase.js
@@ -1,27 +1,27 @@
-// const mongoose = require('mongoose');
-
-// const connectDatabase = () => {
-//     mongoose.connect(process.env.DB_URL).then((con) => {
-//         console.log("MongoDB connected to host: "+con.connection.host);
-//     })
-// };
-
-// module.exports = connectDatabase;
-
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
-    return new Promise((resolve, reject) => {
-        mongoose.connect(process.env.DB_URL)
-        .then((con) => {
-            console.log("MongoDB connected to host: " + con.connection.host);
-            resolve();
-        })
-        .catch((err) => {
-            console.error("MongoDB connection error:", err);
-            reject(err);
-        });
-    });
+    mongoose.connect(process.env.DB_URL).then((con) => {
+        console.log("MongoDB connected to host: "+con.connection.host);
+    })
 };
 
 module.exports = connectDatabase;
+
+// const mongoose = require('mongoose');
+
+// const connectDatabase = () => {
+//     return new Promise((resolve, reject) => {
+//         mongoose.connect(process.env.DB_URL)
+//         .then((con) => {
+//             console.log("MongoDB connected to host: " + con.connection.host);
+//             resolve();
+//         })
+//         .catch((err) => {
+//             console.error("MongoDB connection error:", err);
+//             reject(err);
+//         });
+//     });
+// };
+
+// module.exports = connectDatabase;
